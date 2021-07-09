debec8d1d404cae742355e3deb34ea2007fc4f8b
vermouth <vermouth@xinxinhes-Mac-mini.local>
2021-07-08 17:32:07 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Mouse: {{x}}x{{y}}</p>
+</template>
+
+<script>
+import { useMouse } from '@vueuse/core'
+export default {
+  name: 'App',
+  setup() {
+    return useMouse()
+  }
+}
+</script>
